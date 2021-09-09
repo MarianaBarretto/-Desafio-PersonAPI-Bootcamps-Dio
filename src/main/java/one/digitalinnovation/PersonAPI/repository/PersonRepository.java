f1d1ab936a8061ce5513187c30d7a154f876e03f
MarianaBarretto <mariana_barrett@hotmail.com>
2021-09-09 13:07:16 -0300

    Bootcamps Dio

---
+++ src/main/java/one/digitalinnovation/PersonAPI/repository/PersonRepository.java
@@ -0,0 +1,7 @@
+package one.digitalinnovation.PersonAPI.repository;
+
+import one.digitalinnovation.PersonAPI.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
